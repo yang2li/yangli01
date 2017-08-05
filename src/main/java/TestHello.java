7c4f57602a1ee5af14d47ef96121039a2de628f1
admin <393544233@qq.com>
2017-08-05 12:40:00 +0800

    Initial commit

---
+++ src/main/java/TestHello.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by admin on 2017/8/5.
+ */
+public class TestHello {
+    @Test
+    public void TestHello() {
+        System.out.println("hello");
+    }
+}
